16d5aa01be0f7724de793e545ac844772800b603
Jordan Dixon <jordan.d@me.com>
2017-08-27 18:07:37 +0100

    Initial Commit

---
+++ WebServiceSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServiceSwift4
+//
+//  Created by Jordan Dixon on 27/08/2017.
+//  Copyright © 2017 Jordan Dixon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
